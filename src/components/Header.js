bb23c1ca7c3b676076c48d94242821727a1c8cf3
Casimiro <cp.ciancimino@gmail.com>
2021-03-02 08:55:36 +0100

    edit: Header style, add shopping cart img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import './Header.sass';
+import shoppingCart from '../mocks/img/shopping-cart.png';
 
 function Header({ logo }) {
   return (
     <header className="Header">
-      <img src={ logo } alt="logo"/>
+      <img className="Header__logo" src={ logo } alt="logo"/>
+      <img className="Header__shopping" src={ shoppingCart } alt="shopping-cart"/>
     </header>
   )
 }
